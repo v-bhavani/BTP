60842b5603bba75405963a654298e614574a4ed2
BCStest <132462815+Irfanaj@users.noreply.github.com>
2024-01-30 12:45:18 +0530

    Update main.tf

--- Cloud_foundary/main.tf
+++ Cloud_foundary/main.tf
@@ -21,7 +21,7 @@ provider "btp" {
 }
  
 provider "cloudfoundry" {
-    api_url = https://api.cf.${var.region}.hana.ondemand.com
+    api_url = "https://api.cf.${var.region}.hana.ondemand.com"
     user = var.username
     password = var.password
 }
