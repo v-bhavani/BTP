84f5011a6f18545d11ba711c8021f0b92f1d65d3
v-bhavani <119451594+v-bhavani@users.noreply.github.com>
2025-03-17 12:15:05 +0530

    Update outputs.tf

--- List_subaccount/outputs.tf
+++ List_subaccount/outputs.tf
@@ -1,3 +1,7 @@
-output "subaccount_details" {
-  value = { for sa in data.btp_subaccounts.all.values : sa.id => sa.name }
+output "subaccount_ids" {
+  value = [for sa in data.btp_subaccounts.all.values : sa.id]
+}
+
+output "subaccount_names" {
+  value = [for sa in data.btp_subaccounts.all.values : sa.name]
 }
